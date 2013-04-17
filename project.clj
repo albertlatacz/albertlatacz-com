5b53040fc4b55563867409aec0a0c8d805692830
Albert Latacz <albert.latacz@gmail.com>
2013-04-17 15:35:19 +0100

    deploy to heroku

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject albertlatacz-com "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.5"]]
+                 [ring "1.1.6"]
+                 [compojure "1.1.5"]
+                 [stencil "0.3.2"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler albertlatacz-com.handler/app}
   :profiles
