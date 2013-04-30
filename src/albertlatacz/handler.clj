cbaf1bdb4eddb4c41b92812863ae10479b058829
Albert Latacz <albert.latacz@gmail.com>
2013-04-30 20:55:48 +0100

    redirect instead of render

--- src/albertlatacz/handler.clj
+++ src/albertlatacz/handler.clj
@@ -2,10 +2,11 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
+            [ring.util.response :as resp]
             [albertlatacz.pages :as page]))
 
 (defroutes app-routes
-  (GET "/" [] (page/java-repl))
+  (GET "/" [] (resp/redirect "/java-repl"))
   (GET "/java-actors-1" [] (page/java-actors-1))
   (GET "/java-actors-2" [] (page/java-actors-2))
   (GET "/java-repl" [] (page/java-repl))
