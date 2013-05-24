f753c12fbf94accac92e9b7cba78897878e8f838
Albert Latacz <albert.latacz@gmail.com>
2013-05-24 14:48:13 +0100

    Updated to clojure 1.5.1 + updated libs

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject albertlatacz-com "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Personal web site"
+  :url "http://www.albertlatacz.com/"
   :min-lein-version "2.0.0"
   :aot  'albertlatacz.main
   :main albertlatacz.main
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.1.6"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring "1.1.8"]
                  [compojure "1.1.5"]
                  [stencil "0.3.2"]
-                 [cheshire "5.1.1"]]
-  :plugins [[lein-ring "0.8.2"]]
+                 [cheshire "5.2.0"]]
+  :plugins [[lein-ring "0.8.5"]]
   :ring {:handler albertlatacz.handler/app}
-  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
